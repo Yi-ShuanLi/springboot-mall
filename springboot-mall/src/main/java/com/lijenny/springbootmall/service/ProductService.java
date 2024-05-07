41cca574257d2825c1c027be6fedccda2199c2f0
Yi-ShuanLi <beauty1997724@gmail.com>
2024-05-07 15:48:44 +0800

    商品功能-實作修改商品

--- springboot-mall/src/main/java/com/lijenny/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/lijenny/springbootmall/service/ProductService.java
@@ -7,4 +7,5 @@ import com.lijenny.springbootmall.model.Product;
 public interface ProductService {
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
+    void updateProduct(Integer productId,ProductRequest productRequest);
 }
