76d11c947e83070aa4fadafd430d50116f28b2d4
Yi-ShuanLi <beauty1997724@gmail.com>
2024-05-08 15:37:52 +0800

    商品功能-實作商品查詢列表(排序)

--- springboot-mall/src/main/java/com/lijenny/springbootmall/dao/impl/ProductDaoImpl.java
+++ springboot-mall/src/main/java/com/lijenny/springbootmall/dao/impl/ProductDaoImpl.java
@@ -29,7 +29,7 @@ public class ProductDaoImpl implements ProductDao {
         String sql="SELECT product_id,product_name, category, image_url, price, stock, description, created_date, last_modified_date FROM product WHERE 1=1";
         Map <String ,Object >map=new HashMap <> ();
         if(productQueryParams.getCategory()!=null){
-            sql+=" AND category=:category";
+            sql+=" AND category =:category";
             map.put("category",productQueryParams.getCategory().name());
         }
         //使用jdbc的百分比，要寫在map的值裡面，不能寫在sql裡面，這是spring jebc的限制
@@ -38,6 +38,10 @@ public class ProductDaoImpl implements ProductDao {
             map.put("search","%"+productQueryParams.getSearch()+"%");
         }
 
+        sql=sql+" ORDER BY "+productQueryParams.getOrderBy()+" "+productQueryParams.getSort();
+        // sql injection => 資料庫隱碼攻擊
+        // string account = ' or 1=1; Delete User; ##
+        // select * from User where Account = '' or 1=1 --' and Password = ''
         List <Product> productList=namedParameterJdbcTemplate.query(sql,map,new ProductRowMapper());
         return productList;
     }
