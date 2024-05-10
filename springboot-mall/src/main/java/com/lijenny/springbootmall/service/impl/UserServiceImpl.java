3fa59f1a85ac81be2eb8920cb284c7dd5851a5a9
Yi-ShuanLi <beauty1997724@gmail.com>
2024-05-10 14:27:25 +0800

    帳號功能-註冊新帳號(檢查帳號email並隱藏返回的密碼)

--- springboot-mall/src/main/java/com/lijenny/springbootmall/service/impl/UserServiceImpl.java
+++ springboot-mall/src/main/java/com/lijenny/springbootmall/service/impl/UserServiceImpl.java
@@ -4,17 +4,30 @@ import com.lijenny.springbootmall.dao.UserDao;
 import com.lijenny.springbootmall.dto.UserRegisterRequest;
 import com.lijenny.springbootmall.model.User;
 import com.lijenny.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log= LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        //檢查註冊的email
+        User user=userDao.getUserByEmail(userRegisterRequest.getEmail());
+        if(user != null){
+            log.warn("該email {} 已經被註冊",userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        //創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 
